19bd7ed7bec221ffcce374e0859f5039a1a50c9e
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-11-16 14:03:32 +0100

    Registration

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -23,7 +23,6 @@ class RegistrationFormType extends AbstractType
             ->add('birthday', BirthdayType::class, [
                 'input' => 'datetime_immutable',
                 'widget' => 'single_text',
-                'required' => false,
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
@@ -36,7 +35,19 @@ class RegistrationFormType extends AbstractType
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
             ])
         ;
     }
