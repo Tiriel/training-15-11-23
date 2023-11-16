44c2a8f6f0ef6578a1e51fa248a54c72b97f4ebd
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-11-16 14:10:42 +0100

    Fix registration

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -33,10 +33,8 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'label' => 'Password',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
